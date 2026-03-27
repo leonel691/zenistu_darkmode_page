3e7788661fa4ef759a7e4965b17828bd7c9304ec
leonel <leonelngueaho@gmail.com>
2026-03-27 11:20:50 +0100

    reconstruction

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// const darkIcons=document.getElementsByClassName("dark-mode")
+// const lightIcons=document.getElementsByClassName("light-mode")
+// const buttonLightDark=document.getElementById("dark-light")
+// let lightMode=true
+
+
+
+
+// let counter =0
+// darkIcons.addEventListener(onclick,()=>{
+//     alert("hello")
+// })
+
+
